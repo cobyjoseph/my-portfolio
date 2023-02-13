04c7cf356e1d3c53feca81328b223088cf62e164
Coby Joseph <coby.joseph@gmail.com>
2023-02-13 20:29:47 -0300

    two skyline svgs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Skylines from '../lib/components/Skylines.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Skylines />
