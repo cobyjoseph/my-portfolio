2c33a51afb0664e24e5aaa69f26c1a8ea39f6469
Coby Joseph <coby.joseph@gmail.com>
2023-04-17 19:49:24 -0300

    data passing works

--- src/lib/stores/Cities.ts
+++ src/lib/stores/Cities.ts
@@ -2,23 +2,27 @@ import { writable } from 'svelte/store';
 
 export const cityStore = writable([
       {
-            cityName: '',
-            years: '',
-            text: ''
+            id: 1,
+            cityName: 'San Francisco Bay Area',
+            years: '1992',
+            text: 'Born and raised.'
       },
       {
-            cityName: '',
-            years: '',
-            text: ''
+            id: 2,
+            cityName: 'Ann Arbor, MI',
+            years: '2010 - 14',
+            text: 'Studied philosophy/politics/economics at U-M.\nCo-founded organization that puts on charity concerts and raises $100k+ annually.'
       },
       {
+            id: 3,
             cityName: 'Washington, DC',
             years: '2015 - 17',
-            text: ''
+            text: 'Project Manager for several $1m+ international development projects simultaneously.\nLed business development trips and organized conferences in four different countries.'
       },
       {
+            id: 4,
             cityName: 'Auckland, NZ',
             years: '2018 - 21',
-            text: 'Played key role in analysis and design of transformative urban design and transport projects as a Sustainable Transport Planner'
+            text: 'Played key role in analysis and design of transformative urban design and transport projects as a Sustainable Transport Planner.'
       }
  ])
