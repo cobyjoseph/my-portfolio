fe642b7340309b2ac5c952776245605f1b689811
Coby Joseph <coby.joseph@gmail.com>
2023-04-18 17:22:34 -0300

    slowing down

--- src/lib/components/landing-components/HeroSection.svelte
+++ src/lib/components/landing-components/HeroSection.svelte
@@ -6,49 +6,23 @@
 </svelte:head>
 
 <div class="flex items-center justify-center  w-full relative mt-2   ">
-	<div class="flex  items-center gap-2">
-		<!-- <img
-			src="text/hero-text-v0.3.png"
-			alt=""
-			class="relative bg-cover w-[8rem] flex aspect-auto "
-		/> -->
-
+	<div class="flex  items-center gap-2 sm:grid sm:grid-cols-2">
 		<div class="text-light1 largeClamp font-genSans font-bold  w-full relative justify-center ">
-			<div class="text-center">Hi. I'm Coby.</div>
-			<!-- <div>I'm Coby.</div> -->
+			<div class="text-sm font-extrabold font-satoshi text-[#24FFDA]">Hi. My name is</div>
+			<div class="">Coby Joseph</div>
+			<div class="text-xl relative  flex flex-col font-satoshi font-bold  text-dark2 z-50       ">
+				I design and develop interactive sites for freelancers, businesses, and entrepreneurs.
+			</div>
+		</div>
+		<div class="w-full justify-center  h-[60vh] -mt-[2rem]  ">
+			<spline-viewer
+				loading="eager"
+				url="https://prod.spline.design/SXQN4KXEQXI0xDE8/scene.splinecode"
+			/>
 		</div>
 	</div>
 
-	<div class="flex flex-col relative sm:grid sm:grid-cols-2 ">
-		<div class="text-xl relative  flex flex-col font-satoshi font-bold  text-dark2 z-50       ">
-			I design and develop sites for:
-			<ul class="list-disc list-inside ml-4">
-				<li>small businesses and freelancers</li>
-				<li>professional services companies</li>
-				<li>ambitious entrepreneurs</li>
-			</ul>
-		</div>
-		<!-- <div class="flex flex-col outline align ">
-			<div class="relative flex mt-4  ">
-				<TutorLandingPage />
-			</div>
-	
-			<div class=" relative -z-20 -mt-[10rem]  justify-center flex">
-				<TutorLandingPage />
-			</div>
-	
-			<div class=" relative -z-30  flex -mt-[10rem] justify-end">
-				<TutorLandingPage />
-			</div>
-		</div> -->
-
-	</div>
-</div>
-<div class="w-full justify-center  h-[60vh] -mt-[2rem]  ">
-	<spline-viewer
-		loading="eager"
-		url="https://prod.spline.design/SXQN4KXEQXI0xDE8/scene.splinecode"
-	/>
+	<div class="flex flex-col relative sm:grid sm:grid-cols-2 " />
 </div>
 
 <style>
