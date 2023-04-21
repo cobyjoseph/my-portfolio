e151a3100366a8c7958187b1326fa5bb193d1c41
Coby Joseph <coby.joseph@gmail.com>
2023-04-21 16:34:48 -0300

    using spline runtime

--- src/lib/components/sections/Landing.svelte
+++ src/lib/components/sections/Landing.svelte
@@ -5,22 +5,22 @@
 	import ExamplesSection from '$lib/components/sections/landing-components/ExamplesSection.svelte';
 
 	import { onMount } from 'svelte';
-	// import { Application } from '@splinetool/runtime';
+	import { Application } from '@splinetool/runtime';
 
-	// let canvas;
+	let canvas;
 
-	// onMount(() => {
-	// 	const spline = new Application(canvas);
-	// 	spline.load('https://prod.spline.design/SXQN4KXEQXI0xDE8/scene.splinecode');
-	// });
+	onMount(() => {
+		const spline = new Application(canvas);
+		spline.load('https://prod.spline.design/SXQN4KXEQXI0xDE8/scene.splinecode');
+	});
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<script
 		type="module"
 		src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"
 	></script>
-</svelte:head>
+</svelte:head> -->
 
 
 
@@ -30,11 +30,11 @@
 		<HeroSection />
 	</div>
 
-	<spline-viewer
+	<!-- <spline-viewer
 		class="h-full outline"
 		url="https://prod.spline.design/SXQN4KXEQXI0xDE8/scene.splinecode"
-	/>
+	/> -->
 
-	<!-- <canvas id="canvas3d" bind:this={canvas} class="outline outline-red-500  sm:my-auto sm:ml-auto" /> -->
+	<canvas id="canvas3d" bind:this={canvas} class="outline outline-red-500  sm:my-auto sm:ml-auto" />
 </div>
 
