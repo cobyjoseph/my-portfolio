d873dab08fb59107248b428f3cb446a019bfc7b4
Coby Joseph <coby.joseph@gmail.com>
2023-06-28 14:08:40 -0400

    updated meta tags and title for SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,11 @@
 		href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,301,701,300,501,401,901,400&f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&f[]=switzer@600,700,400,500&f[]=neco@500,501,401,700,901,900,701,400&f[]=ranade@700,300,701,401,400,500,501,301&display=swap"
 		rel="stylesheet"
 	/>
+	<title>Coby Joseph - Freelance Web Designer & Developer</title>
+	<meta
+		name="description"
+		content="Coby Joseph, a freelance web designer and developer, creates interactive, custom, and blazingly quick websites using HTML, CSS, Javascript, Svelte, and Sveltekit. With a background as a Project Manager, he values detail and clear communication in all his work."
+	/>
 </svelte:head>
 
 <div class="px-[8%]">
